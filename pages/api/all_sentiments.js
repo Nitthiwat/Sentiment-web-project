c531409b62d0180fd5fdb9e16af67f0fafddca62
Nitthiwat <nitthiwat.j@kkumail.com>
2024-03-20 15:24:33 +0700

    final edit

--- pages/api/all_sentiments.js
+++ pages/api/all_sentiments.js
@@ -1,15 +1,6 @@
 import clientPromise from "../../lib/mongodb";
-// import Cors from 'cors';
-// import initMiddleware from '../../lib/init-middleware';
-
-// const cors = initMiddleware(
-//     Cors({
-//         methods: ['GET', 'POST', 'OPTIONS'],
-//     })
-// );
 
 export default async function handler(req, res) {
-    // await cors(req, res);
 
     try {
         const client = await clientPromise;
